262e1ab3123501355062c73537af3cb3a54836b5
dekokun <dekokun@hatena.ne.jp>
2021-12-08 10:32:24 +0900

    add vpc

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,16 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'Vpc', { maxAzs: 1 });
   }
 }
