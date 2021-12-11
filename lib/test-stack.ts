c0a04f7ae7a94b80373229669814a8454553e989
dekokun <dekokun@hatena.ne.jp>
2021-12-11 22:57:25 +0900

    micro is enough

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -25,7 +25,7 @@ export class TestStack extends Stack {
       }],
       securityGroup: sg,
       instanceName: "bitcoin-full-node",
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.SMALL)
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MICRO)
     });
 
     const lb = new elbv2.NetworkLoadBalancer(this, 'lb', { vpc, internetFacing: true });
